c2e32cca1dd1ee0f9b7f0c3f8ecdf967bdc9de1a
Aliphon <redshiftkeying@gmail.com>
2018-12-05 15:10:15 +0800

    add client src & bin

---
+++ client/src/components/Footer.vue
@@ -0,0 +1,11 @@
+<template>
+  <footer class="app-footer">
+    <a >档案管理系统</a> &copy; 2018 山西通畅. 
+    <span class="float-right">Powered by <a href="http://itji.net">ITJI</a></span>
+  </footer>
+</template>
+<script>
+export default {
+  name: 'footer'
+}
+</script>
