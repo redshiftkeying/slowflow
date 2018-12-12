ce69de35bdb6a11c792d409844fbe0f147246bbb
ACS <redshiftkeying@gmail.com>
2018-12-12 09:13:33 +0000

    fix mysql config

--- server/models/base.go
+++ server/models/base.go
@@ -7,5 +7,5 @@ import (
 
 func init() {
 	orm.RegisterDriver("mysql", orm.DRMySQL)
-	orm.RegisterDataBase("default", "mysql", "user-zKKrHv6txr:8*PKKNP~y`Ft&TZIiTL-@tcp(mysql.coding.io:3306)/db-JoeXsz4uC5?charset=utf8")
+	orm.RegisterDataBase("default", "mysql", "some config for your")
 }
